1b0145927bc9bef389253c43183919ef906a528b
Mikhail Anishchenko <misha321654897i@gmail.com>
2025-02-06 23:55:43 +0300

    init coomit (lab is almost done)

---
+++ lab/lab.cpp
@@ -0,0 +1,119 @@
+﻿#include <windows.h>
+#include <xstring>
+
+#include "Resource.h"
+#include "framework.h"
+#include "Renderer.h"
+
+
+constexpr int start_w = 1280;
+constexpr int start_h = 720;
+
+HINSTANCE       g_hInst = nullptr;
+HWND            g_hWnd = nullptr;
+
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
+{
+    WNDCLASSEX wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LAB));
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_LAB));
+    wcex.hIconSm = nullptr;
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = nullptr;
+    wcex.lpszClassName = L"Window";
+    if (!RegisterClassEx(&wcex))
+        return E_FAIL;
+
+    g_hInst = hInstance;
+    RECT rc = { 0, 0, start_w, start_h };
+    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+    g_hWnd = CreateWindow(L"Window", L"Anishchenko Mikhail",
+        WS_OVERLAPPED | WS_CAPTION | WS_THICKFRAME | WS_SYSMENU | WS_MINIMIZEBOX,
+        CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
+        nullptr);
+    if (!g_hWnd)
+        return E_FAIL;
+
+    ShowWindow(g_hWnd, nCmdShow);
+    SetForegroundWindow(g_hWnd);
+    SetFocus(g_hWnd);
+
+    return S_OK;
+}
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    if (FAILED(InitWindow(hInstance, nCmdShow)))
+        return 0;
+
+    if (FAILED(Renderer::GetInstance().Init(g_hWnd, g_hInst, start_w, start_h)))
+    {
+        Renderer::GetInstance().CleanupDevice();
+        return 0;
+    }
+
+    MSG msg = { 0 };
+    while (WM_QUIT != msg.message)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        if (Renderer::GetInstance().Frame()) Renderer::GetInstance().Render();
+    }
+
+    Renderer::GetInstance().CleanupDevice();
+
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    switch (message)
+    {
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+        break;
+
+
+    case WM_GETMINMAXINFO:
+    {
+        LPMINMAXINFO lpMMI = (LPMINMAXINFO)lParam;
+        lpMMI->ptMinTrackSize.x = 256;
+        lpMMI->ptMinTrackSize.y = 256;
+        break;
+    }
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    case WM_SIZE:
+        Renderer::GetInstance().ResizeWindow(g_hWnd);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
