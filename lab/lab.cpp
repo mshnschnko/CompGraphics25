dfc26e7c2f660c4ac2320b93c960f7f48c8102fe
Mikhail Anishchenko <misha321654897i@gmail.com>
2025-02-09 13:27:14 +0300

    rename window

--- lab/lab.cpp
+++ lab/lab.cpp
@@ -38,7 +38,7 @@ HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
     g_hInst = hInstance;
     RECT rc = { 0, 0, start_w, start_h };
     AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-    g_hWnd = CreateWindow(L"Window", L"Anishchenko Mikhail",
+    g_hWnd = CreateWindow(L"Window", L"Lab 1",
         WS_OVERLAPPED | WS_CAPTION | WS_THICKFRAME | WS_SYSMENU | WS_MINIMIZEBOX,
         CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
         nullptr);
